e7bc70627ef7c422e630f4b028ac0fa141bdb18a
LE SECH Yann <yann.le-sech@imt-atlantique.net>
2025-11-27 10:11:53 +0100

    création de la classe vecteur3d

--- msckf/msckf.h
+++ msckf/msckf.h
@@ -1,16 +1,20 @@
-
+#include "quaternion.h"
 
 
 
 
 class MSCKF {
     public:
-        // constructeur par défault
+        // constructeur par défaut
         MSCKF();
 
     private:
         float pos;
         float vit;
+        Quaternion quaternion;
+        float bg;
+        float ba;
+
 
 
 };
